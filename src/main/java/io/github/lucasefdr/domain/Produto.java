0b0efdf3f68d9d7cbc08608d6f031b4c26c01421
Lucas <lucas.efdrosa@gmail.com>
2023-01-05 21:32:23 -0300

    Entidade Produto criada

---
+++ src/main/java/io/github/lucasefdr/domain/Produto.java
@@ -0,0 +1,47 @@
+package io.github.lucasefdr.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    public String nome;
+    public String descricao;
+    public BigDecimal decimal;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getDecimal() {
+        return decimal;
+    }
+
+    public void setDecimal(BigDecimal decimal) {
+        this.decimal = decimal;
+    }
+}
